f3b9c94bccc5b8f7dbb8611d59bcdc67460125ff
David Pedersen <limero@me.com>
2023-02-22 20:22:10 +0100

    Replace vim plug with lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,8 +3,4 @@ vim.cmd('source ~/.vimrc')
 
 require('terminal')
 require('plugins')
-if vim.fn.executable('fzf') == 1 then
-  require('fzf')
-end
-require('gitsigns').setup()
 require('lsp')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,24 @@
-vim.cmd([[
-  call plug#begin()
-  Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-  Plug 'junegunn/fzf.vim'
-  Plug 'lewis6991/gitsigns.nvim'
-  "Plug 'wellle/context.vim'
-  call plug#end()
-]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  'junegunn/fzf',
+  'junegunn/fzf.vim',
+  'lewis6991/gitsigns.nvim',
+  --'wellle/context.vim',
+})
+
+require('gitsigns').setup()
+if vim.fn.executable('fzf') == 1 then
+  require('fzf')
+end
