018bd20ad35ba0d904f380bfe4c78a5b6369af6e
David Pedersen <limero@me.com>
2023-07-25 23:59:23 +0200

    Add cpp lsp

---
+++ .config/nvim/ftplugin/cpp.lua
@@ -0,0 +1,10 @@
+local path = vim.fs.find({ ".clang-format" }, { type = "file" })
+vim.lsp.start({
+    name = "clangd",
+    cmd = { "clangd" },
+    root_dir = vim.fs.dirname(path[1]),
+    settings = {
+        ["clangd"] = {
+        },
+    },
+})
