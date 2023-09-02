72daeacf57fcd87283c26ae49c048675084a7d1c
David Pedersen <limero@me.com>
2023-09-02 19:06:33 +0200

    Remove unused nvim context plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,7 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { 'junegunn/fzf.vim',        event = "VeryLazy" },
   { 'lewis6991/gitsigns.nvim', event = "VeryLazy" },
-  --{'wellle/context.vim', event = "VeryLazy" },
 })
 
 require('plugins.gitsigns')
