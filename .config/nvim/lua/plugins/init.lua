7452f9e5a54962ca5ea990851f38589577df6dc0
David Pedersen <limero@me.com>
2023-10-20 00:19:02 +0900

    Remove comment and treesitter plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,52 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { 'junegunn/fzf.vim',        event = "VeryLazy" },
   { 'lewis6991/gitsigns.nvim', event = "VeryLazy" },
-  {
-    'numToStr/Comment.nvim',
-    opts = {
-      toggler = {
-        line = '<Leader>/',
-      },
-      opleader = {
-        line = '<Leader>/',
-      },
-    },
-    lazy = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = {
-          "bash",
-          "go",
-          "json",
-          "lua",
-          "luadoc",
-          "luap",
-          "markdown",
-          "markdown_inline",
-          "vim",
-          "vimdoc",
-          "yaml",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<Leader>s",
-            node_incremental = "<Leader>s",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
 })
 
 require('plugins.gitsigns')
