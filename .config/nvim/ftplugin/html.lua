2577f0ad95de8ebcb5df5a91f6cb5a00a47704bf
David Pedersen <limero@me.com>
2024-03-26 23:40:36 +0100

    Add ftplugin for html

---
+++ .config/nvim/ftplugin/html.lua
@@ -0,0 +1,10 @@
+vim.opt.expandtab = true
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("Format on save", {}),
+  callback = function()
+    vim.api.nvim_command('normal gg=G')
+    vim.cmd([[exe "normal \<C-o>"]])
+  end,
+})
+
