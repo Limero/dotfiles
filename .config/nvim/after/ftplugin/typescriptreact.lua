f4fb66b1e31219bf99a1b58a980ea95348deab63
David Pedersen <limero@me.com>
2024-12-26 15:31:13 +0700

    Add lsp config for ts

---
+++ .config/nvim/after/ftplugin/typescriptreact.lua
@@ -0,0 +1,11 @@
+if vim.fn.executable('typescript-language-server') == 0 then
+  print('typescript-language-server is missing')
+  return
+end
+
+vim.lsp.start({
+  name = "tsserver",
+  cmd = { "typescript-language-server", "--stdio" },
+  root_dir = vim.fs.root(0, 'package.json'),
+  settings = {},
+})
