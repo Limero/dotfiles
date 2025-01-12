47d3fb7d62bd94330ddac931f2b76b8b35275279
David Pedersen <limero@me.com>
2025-01-12 19:17:54 +0700

    Fix nvim yt function on Fedora Atomic

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -9,11 +9,12 @@ vim.opt.listchars = {
 vim.opt.expandtab = false
 
 vim.keymap.set('n', 'yt', function()
+  local dirPath = vim.fn.expand("%:p:h"):gsub('^/var', '')
   local testLine = vim.fn.getline('.'):match('func%s+([%w_]+)')
   if testLine then
-    vim.fn.setreg('+', "go test -run ^" .. testLine .. "$ " .. vim.fn.expand("%:p:h"))
+    vim.fn.setreg('+', "go test -run ^" .. testLine .. "$ " .. dirPath)
   else
-    vim.fn.setreg('+', "go test " .. vim.fn.expand("%:p:h"))
+    vim.fn.setreg('+', "go test " .. dirPath)
   end
 end)
 
