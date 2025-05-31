673fa006cf0d499b7076c44b48c8f14c6f37f3db
David Pedersen <limero@me.com>
2025-05-31 15:44:18 +0200

    Use single quotes in luals

--- .config/nvim/after/lsp/lua_ls.lua
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -4,6 +4,12 @@ return {
   root_dir = vim.fs.root(0, { 'init.lua', '.git' }),
   settings = {
     Lua = {
+      format = {
+        enable = true,
+        defaultConfig = {
+          quote_style = 'single',
+        }
+      },
       runtime = {
         version = 'LuaJIT',
       },
