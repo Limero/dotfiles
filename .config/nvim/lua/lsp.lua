8c7f34c97bd6d1102be8ad821222aea7b9cd3073
David Pedersen <limero@me.com>
2025-06-02 18:53:39 +0200

    Add uppercase letters to completion trigger chars

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -71,7 +71,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     if client:supports_method('textDocument/completion') then
-      client.server_capabilities.completionProvider.triggerCharacters = vim.split('qwertyuiopasdfghjklzxcvbnm.', '')
+      client.server_capabilities.completionProvider.triggerCharacters = vim.split(
+        'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM.', '')
       vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
     end
 
