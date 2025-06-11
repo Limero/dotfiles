5740d59a5506280b5a68d0ec4aa4c4f0905f10c1
David Pedersen <limero@me.com>
2025-06-11 13:29:50 +0200

    Simplify yt function dirPath

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -6,7 +6,7 @@ vim.opt.listchars = {
 }
 
 vim.keymap.set('n', 'yt', function()
-  local dirPath = vim.fn.expand("%:p:h"):gsub('.*/go', '~/go')
+  local dirPath = './' .. vim.fn.expand("%:h:.")
   local testLine = vim.fn.getline('.'):match('func%s+([%w_]+)')
   if testLine then
     vim.fn.setreg('+', "go test -run ^" .. testLine .. "$ " .. dirPath)
