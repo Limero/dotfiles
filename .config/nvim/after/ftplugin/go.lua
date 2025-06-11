516268be8400e2655ca3fc5acfd4af4b59214a7e
David Pedersen <limero@me.com>
2025-06-11 13:20:21 +0200

    Fix dirPath for yt function

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -6,7 +6,7 @@ vim.opt.listchars = {
 }
 
 vim.keymap.set('n', 'yt', function()
-  local dirPath = vim.fn.expand("%:p:h"):gsub('^/var', '')
+  local dirPath = vim.fn.expand("%:p:h"):gsub('.*/go', '~/go')
   local testLine = vim.fn.getline('.'):match('func%s+([%w_]+)')
   if testLine then
     vim.fn.setreg('+', "go test -run ^" .. testLine .. "$ " .. dirPath)
