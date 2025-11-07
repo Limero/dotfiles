a8aa2c54742a2d8d8ecd87ab0a013bfe08b314f3
David Pedersen <limero@me.com>
2025-11-07 19:45:26 +0100

    Add lsp for Odin

---
+++ .config/nvim/after/lsp/ols.lua
@@ -0,0 +1,10 @@
+return {
+  cmd = { 'ols' },
+  filetypes = { 'odin' },
+  root_dir = vim.fs.root(0, { 'ols.json', '.git', '*.odin' }),
+  init_options = {
+    enable_semantic_tokens = true,
+    enable_auto_import = true,
+    odin_command = '~/odin/odin',
+  },
+}
