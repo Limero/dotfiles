16a8ede39c6b3ac7dc25fced86888084ce059686
David Pedersen <limero@me.com>
2026-04-13 13:13:46 +0200

    Use lsp client offset encoding

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -44,13 +44,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
         if vim.bo.filetype == 'lua' then return end
 
         -- https://github.com/neovim/nvim-lspconfig/issues/115
-        local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+        local params = vim.lsp.util.make_range_params(0, client.offset_encoding)
         params.context = { only = { 'source.organizeImports' } }
         local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, 5000)
         for _, res in pairs(result or {}) do
           for _, r in pairs(res.result or {}) do
             if r.edit then
-              vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+              vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
             else
               vim.lsp.buf.execute_command(r.command)
             end
