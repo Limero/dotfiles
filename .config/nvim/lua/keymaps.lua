4e10170de22e0f6f574eba990824f7dd7bcef35c
David Pedersen <limero@me.com>
2026-05-21 23:50:50 +0200

    Improve copy path functions

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,9 +7,33 @@ vim.keymap.set('x', 'p', '"_dP')
 vim.keymap.set('i', '{<CR>', '{<CR>}<ESC>O') -- Automatically add closing curly brace
 vim.keymap.set('i', '(<CR>', '(<CR>)<ESC>O') -- Automatically add closing parenthesis
 
--- Yank file name/path of current buffer
--- https://stackoverflow.com/questions/916875/yank-file-name-path-of-current-buffer-in-vim
--- https://stackoverflow.com/questions/4525261/getting-relative-paths-in-vim/24463362#24463362
-vim.keymap.set('n', 'yp', [[:let @+ = expand("%:~:.")<CR>]])
-vim.keymap.set('n', 'yfp', [[:let @+ = expand("%:p")<CR>]])
-vim.keymap.set('n', 'yd', [[:let @+ = expand("%:h:.").'/'<CR>]])
+-- Put relative path of current buffer's file in clipboard
+vim.keymap.set('n', 'yp', function()
+  vim.fn.setreg('+', vim.fn.expand('%:~:.'))
+end)
+
+-- Put relative path and cursor line number of current buffer's file in clipboard
+vim.keymap.set('n', 'yrr', function()
+  vim.fn.setreg('+', vim.fn.expand('%:~:.')..':'..vim.api.nvim_win_get_cursor(0)[1])
+end)
+
+-- Put full path of current buffer's file in clipboard
+vim.keymap.set('n', 'yfp', function()
+  vim.fn.setreg('+', vim.fn.expand('%:p'))
+end)
+
+-- Put relative path of current buffer's directory in clipboard
+vim.keymap.set('n', 'yd', function()
+  vim.fn.setreg('+', vim.fn.expand('%:h:.'))
+end)
+
+-- Put GitHub/GitLab link to buffer's file in clipboard
+vim.keymap.set('n', 'ygg', function()
+  local git_branch = vim.fn.system('git branch --show-current'):gsub('\n', '')
+  local url = 'https://'..vim.fn.system("git remote get-url origin 2>/dev/null")
+    :gsub('\n', '')
+    :gsub('^.-@', '', 1)
+    :gsub(':', '/', 1)
+    :gsub('%.git$', '/blob/'..git_branch..'/'..vim.fn.expand('%:~:.'))
+  vim.fn.setreg('+', url)
+end)
